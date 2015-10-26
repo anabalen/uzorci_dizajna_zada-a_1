60770eb50f16f37260b6b1d163575c4a632f8d9d
anabalen <balen.anamarija@gmail.com>
2015-10-26 18:41:11 +0100

    ..

--- src/foi/uzdiz/anabalen/zadaca1/abstractFactory/CreateCameras.java
+++ src/foi/uzdiz/anabalen/zadaca1/abstractFactory/CreateCameras.java
@@ -5,6 +5,7 @@
  */
 package foi.uzdiz.anabalen.zadaca1.abstractFactory;
 
+import foi.uzdiz.anabalen.abstractfactory.interfaces.Compact;
 import foi.uzdiz.anabalen.abstractfactory.interfaces.DSLM;
 import foi.uzdiz.anabalen.abstractfactory.interfaces.DSLR;
 
@@ -15,46 +16,65 @@ import foi.uzdiz.anabalen.abstractfactory.interfaces.DSLR;
 public class CreateCameras {
     
     public void createCameras(){
-        //get DSLR factory
-      AbstractFactory dslrFactory = FactoryProducer.getFactory("DSLR");
+     //get DSLR factory
+        AbstractFactory dslrFactory = FactoryProducer.getFactory("DSLR");
 
-      DSLR dslr1;
-        dslr1 = dslrFactory.getDSLR("CanonEOS700D");
+        DSLR dslr1 = dslrFactory.getDSLR("CanonEOS700D");
 
-      //call draw method of Shape Circle
-      dslr1.makeDSLR();
+        //call make method of DSLR camera1
+        dslr1.makeDSLR();
 
-      DSLR dslr2;
-        dslr2 = dslrFactory.getDSLR("NikonD3200");
+        DSLR dslr2 = dslrFactory.getDSLR("NikonD3200");
 
-      //call draw method of Shape Rectangle
-      dslr2.makeDSLR();
-      
-      DSLR dslr3;
-        dslr3 = dslrFactory.getDSLR("CanonEOS100D");
+        //call make method of DSLR camera2
+        dslr2.makeDSLR();
 
-      //call draw method of Shape Square
-      dslr3.makeDSLR();
+        DSLR dslr3 = dslrFactory.getDSLR("CanonEOS100D");
 
-      //get color factory
-      AbstractFactory dslmFactory = FactoryProducer.getFactory("DSLM");
+        //call make method of DSLR camera3
+        dslr3.makeDSLR();
 
-      //get an object of Color Red
-      DSLM  dslm1 = dslmFactory.getDSLM("LumixG6H");
+        //get DSLM factory
+        AbstractFactory dslmFactory = FactoryProducer.getFactory("DSLM");
 
-      //call fill method of Red
-      dslm1.makeDSLM();
+        
+        DSLM dslm1 = dslmFactory.getDSLM("LumixG6H");
+        
+        //call make method of DSLM camera1
+        dslm1.makeDSLM();
 
-      //get an object of Color Green
-      DSLM  dslm2 = dslmFactory.getDSLM("OlympusEM10");
+        
+        DSLM dslm2 = dslmFactory.getDSLM("OlympusEM10");
+        
+        //call make method of DSLM camera2
+        dslm2.makeDSLM();
 
-      //call fill method of Green
-      dslm2.makeDSLM();
+        
+        DSLM dslm3 = dslmFactory.getDSLM("SonyA5000");
+        
+        //call make method of DSLM camera3
+        dslm3.makeDSLM();
+        
+        
+        //get Compact factory
+        AbstractFactory compactFactory = FactoryProducer.getFactory("Compact");
 
-      //get an object of Color Blue
-      DSLM  dslm3 = dslmFactory.getDSLM("SonyA5000");
+        
+        Compact compact1 = compactFactory.getCompact("NikonCoolPixS33");
+        
+        //call make method of DSLM camera1
+        compact1.makeCompact();
 
-      //call fill method of Color Blue
-      dslm3.makeDSLM();
+        
+        Compact compact2 = compactFactory.getCompact("SonyDSC200W");
+        
+        //call make method of DSLM camera2
+        compact2.makeCompact();
+
+        
+        Compact compact3 = compactFactory.getCompact("CanonIXUS170");
+        
+        //call make method of DSLM camera3
+        compact3.makeCompact();
     }
 }
